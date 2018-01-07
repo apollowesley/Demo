b9bb056bbd905cf7bc4523e9a78bb8f512d356a3
chenboxing <36821277@qq.com>
2018-01-07 18:35:16 +0800

    sql server

--- python/sqlserver/demo/sqlserver.py
+++ python/sqlserver/demo/sqlserver.py
@@ -1,44 +1,57 @@
 # -*- coding:utf-8 -*-
 
+# Server=(local);Database=DaysNetPortal;uid=DaysPortal;pwd=pass@word1
+
 import pymssql
 
+
 class MSSQL:
-    def __init__(self,host,user,pwd,db):
+    def __init__(self, host, user, pwd, db):
         self.host = host
         self.user = user
-        self.pwd  = pwd
-        self.db   = db
+        self.pwd = pwd
+        self.db = db
 
     def __GetConnect(self):
         if not self.db:
-            raise(NameError,"没有设置数据库信息")
-        self.conn = pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset="utf8")
+            raise(NameError, "没有设置数据库信息")
+        self.conn = pymssql.connect(
+            host=self.host, user=self.user, password=self.pwd, database=self.db, charset="utf8")
         cur = self.conn.cursor()
         if not cur:
-            raise(NameError,"连接数据库失败")
+            raise(NameError, "连接数据库失败")
         else:
             return cur
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         resList = cur.fetchall()
 
-        #查询完毕后必须关闭连接
+        # 查询完毕后必须关闭连接
         self.conn.close()
         return resList
 
-    def ExecNonQuery(self,sql):
+    def ExecNonQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         self.conn.commit()
         self.conn.close()
 
-ms = MSSQL(host="192.168.1.1",user="sa",pwd="sa",db="testdb")
-reslist = ms.ExecQuery("select * from webuser")
-for i in reslist:
-    print i
 
-newsql="update webuser set name='%s' where id=1"%u'测试'
-print newsql
-ms.ExecNonQuery(newsql.encode('utf-8'))
+ms = MSSQL(host="211.102.91.212", user="DaysPortal",
+           pwd="pass@word1", db="DaysNetPortal")
+reslist = ms.ExecQuery("select top 5 *  from Main_Product")
+for i in reslist:
+    # chardet.detect(i[1])
+    # encode('latin1').decode('gbk')
+    for k in i:
+        print k
+
+    # print i[1]
+    # print i[1].encode('latin1').decode('gbk')
+    # print i
+
+# newsql = "update webuser set name='%s' where id=1" % u'测试'
+# print newsql
+# ms.ExecNonQuery(newsql.encode('utf-8'))
